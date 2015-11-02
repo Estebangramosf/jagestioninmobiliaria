e966d4d389cd5ede38eb1a65b55186a879fee9d6
estebangramosf <esteban.ramos.f@gmail.com>
2015-11-02 17:00:11 -0300

    Formulario de contacto

--- contacto.php
+++ contacto.php
@@ -1,14 +1,13 @@
 
 <?php 
-$tituloPagina = "Contacto";
-$pagina = "nosotros";
-include('inc/header.php');
-//include('inc/carousel.php') ?>
-<?php
-function dameURL(){
-$url="http://".$_SERVER['HTTP_HOST'].":".$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI'];
-return $url;
-}
+  $tituloPagina = "Contacto";
+  $pagina = "nosotros";
+  include('inc/header.php');  
+  
+  function dameURL(){
+  $url="http://".$_SERVER['HTTP_HOST'].":".$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI'];
+  return $url;
+  }
 ?>
 <div id="content" class="container">
   <div class="row">
@@ -23,35 +22,49 @@ return $url;
     hipotecario de forma r&aacute;pida y eficaz, <strong>no te pierdas esta oportunidad.</strong><br>Los datos los puedes encontrar a continuac&oacute;n del formulario de mas abajo.-</h3>
   </div>
   <div id="contentIn">
+    <!-- Formulario de contacto -->
     <form class="form" method="post" action="enviarporMail.php">
       <div class="form">
-          <div class="form-group">
-            <input name="name" type="text" required="" class="form-control" id="name" placeholder="Tu Nombre *" data-validation-required-message="Ingrese su nombre.">
-            <p class="help-block text-danger"></p>
-          </div>
-          <div class="form-group">
-            <input name="email" type="email" required="" class="form-control" id="email" placeholder="Tu E-mail *" data-validation-required-message="Ingrese su dirección de e-mail.">
-            <p class="help-block text-danger"></p>
-          </div>
-          <div class="form-group">
-            <input name="telefono" type="text" required="" class="form-control" id="telefono" placeholder="Tu n&uacute;mero telef&oacute;nico *" data-validation-required-message="Ingrese su n&uactue;mero telef&oacute;nico.">
-            <p class="help-block text-danger"></p>
-          </div>            
-          <div class="form-group">
-            <textarea name="message" required="" class="form-control" id="message" placeholder="Tu Mensaje *" data-validation-required-message="Ingrese su Mensaje."></textarea>
-            <p class="help-block text-danger"></p>
-          </div>
-          <div class="form-group">
-                <input name="url" type="hidden" required="" class="form-control" id="url" placeholder="" value="<?php echo dameURL(); ?>" data-validation-required-message="Ingrese su n&uactue;mero telef&oacute;nico.">
-                <p class="help-block text-danger"></p>
-            </div>            
-          <div class="form-group">
-            <button class="btn btn-group-justified btn-success" id="submitButton">Enviar consulta</button>
-          </div>
+        <!-- Campo nombre -->
+        <div class="form-group">
+          <input name="name" type="text" required="" class="form-control" id="name" placeholder="Tu Nombre *" data-validation-required-message="Ingrese su nombre.">
+          <p class="help-block text-danger"></p>
+        </div>
+        <!-- /Campo nombre -->
+        <!-- Campo email -->
+        <div class="form-group">
+          <input name="email" type="email" required="" class="form-control" id="email" placeholder="Tu E-mail *" data-validation-required-message="Ingrese su dirección de e-mail.">
+          <p class="help-block text-danger"></p>
+        </div>
+        <!-- /Campo email -->
+        <!-- Campo telefono -->
+        <div class="form-group">
+          <input name="telefono" type="text" required="" class="form-control" id="telefono" placeholder="Tu n&uacute;mero telef&oacute;nico *" data-validation-required-message="Ingrese su n&uactue;mero telef&oacute;nico.">
+          <p class="help-block text-danger"></p>
+        </div>  
+        <!-- /Campo telefono -->          
+        <!-- Campo mensaje -->
+        <div class="form-group">
+          <textarea name="message" required="" class="form-control" id="message" placeholder="Tu Mensaje *" data-validation-required-message="Ingrese su Mensaje."></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <!-- /Campo mensaje -->
+        <!-- Campo url -->
+        <div class="form-group">
+              <input name="url" type="hidden" required="" class="form-control" id="url" placeholder="" value="<?php echo dameURL(); ?>" data-validation-required-message="Ingrese su n&uactue;mero telef&oacute;nico.">
+              <p class="help-block text-danger"></p>
+        </div>         
+        <!-- /Campo url -->   
+        <!-- Boton submit -->
+        <div class="form-group">
+          <button class="btn btn-group-justified btn-success" id="submitButton">Enviar consulta</button>
+        </div>
+        <!-- /Boton submit -->
       </div>
-  </form>  
+    </form> 
+    <!-- /Formulario de contacto --> 
 </div>  
-<div id="contentIn">
+<div id="contentIn" style="text-align:center;">
   <h3>Contacta con nuestra ejecutiva externa directamente</h3>
   <table style="text-align=center;"> 
     <tr>
@@ -64,7 +77,7 @@ return $url;
     </tr>
     <tr>
       <td>Correo electr&oacute;nico</td>  
-      <td><a class="btn btn-default" href="mailto:dapolancoc@ecomac.cl">dapolancoc@ecomac.cl</a></td>
+      <td><a class="btn btn-default" target="_blank" href="mailto:dapolancoc@ecomac.cl">dapolancoc@ecomac.cl</a></td>
     </tr>        
   </table>
 </div>   
